aa682210a3341360663fa44a49841ee4dd4e8529
Aditya Agarwal <50960175+adi611@users.noreply.github.com>
2022-02-20 22:02:59 +0530

    subseq

---
+++ Day 2/Count subsequences of type a^i, b^j, c^k/GFG.java
@@ -0,0 +1,52 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.next());
+
+        while (t > 0) {
+            String s = sc.next();
+
+            Solution T = new Solution();
+            System.out.println(T.fun(s));
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    public int fun(String str) {
+        int n = str.length();
+        int subsequences[][] = new int[3][n + 1];
+        int M = 1000000007;
+        for (int stringPos = 1; stringPos <= n; stringPos++) {
+            ;
+            char ch = str.charAt(stringPos - 1);
+            if (ch == 'a') {
+                subsequences[0][stringPos] = ((2 * subsequences[0][stringPos - 1]) % M + 1) % M;
+                subsequences[1][stringPos] = subsequences[1][stringPos - 1];
+                subsequences[2][stringPos] = subsequences[2][stringPos - 1];
+            } else if (ch == 'b') {
+                subsequences[1][stringPos] = ((2 * subsequences[1][stringPos - 1]) % M + subsequences[0][stringPos - 1])
+                        % M;
+                subsequences[0][stringPos] = subsequences[0][stringPos - 1];
+                subsequences[2][stringPos] = subsequences[2][stringPos - 1];
+            } else {
+                subsequences[2][stringPos] = ((2 * subsequences[2][stringPos - 1]) % M + subsequences[1][stringPos - 1])
+                        % M;
+                subsequences[0][stringPos] = subsequences[0][stringPos - 1];
+                subsequences[1][stringPos] = subsequences[1][stringPos - 1];
+            }
+        }
+        return (subsequences[2][n]);
+    }
+}
